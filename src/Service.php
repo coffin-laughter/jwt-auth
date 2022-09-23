00eeb7d0db9659fa06861fe6cf3783c632d840aa
coffin_laughter <chuanshuo_yongyuan@163.com>
2022-09-23 19:25:46 +0800

    init

--- src/Service.php
+++ src/Service.php
@@ -2,9 +2,12 @@
 
 namespace coffin\jwtauth;
 
-use coffin\jwtauth\middleware\InjectJwt;
 use coffin\jwtauth\provider\ThinkPHP6ServiceProvider;
 
-class Service extends ThinkPHP6ServiceProvider
+class Service extends \think\Service
 {
+    public function boot()
+    {
+        (new ThinkPHP6ServiceProvider($this->app))->register();
+    }
 }
