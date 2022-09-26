16726f5f97f821498a80b48bd6288e5c0d464fad
coffin_laughter <chuanshuo_yongyuan@163.com>
2022-09-26 15:42:03 +0800

    init

--- src/Service.php
+++ src/Service.php
@@ -2,6 +2,7 @@
 
 namespace coffin\jwtauth;
 
+use coffin\jwtauth\command\SecretCommand;
 use coffin\jwtauth\provider\ThinkPHP6ServiceProvider;
 
 class Service extends \think\Service
@@ -9,5 +10,6 @@ class Service extends \think\Service
     public function boot()
     {
         (new ThinkPHP6ServiceProvider($this->app))->register();
+        $this->commands(SecretCommand::class);
     }
 }
